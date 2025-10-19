6ee41dcaf59342de7448853558941fc76b1629e1
josef-karpinski <josef.karpinski@uconn.edu>
2025-10-19 01:00:27 -0400

    fix: vite base url for gh-pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import { ApiTokenProvider } from './components/ApiTokenProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <ApiTokenProvider>
         <App />
       </ApiTokenProvider>
